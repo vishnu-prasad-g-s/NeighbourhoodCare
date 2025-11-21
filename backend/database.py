bf3225321ee0743877db5b0858dbdaab3bc4657e
vishnu-prasad-g-s <vishnuprasadgs08@gmail.com>
2025-11-21 20:23:14 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,33 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("neighbourhoodcare.db")
+    cursor = conn.cursor()
+
+    # Requests table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS requests (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        phone TEXT,
+        address TEXT,
+        task TEXT,
+        urgency TEXT,
+        status TEXT DEFAULT 'pending'
+    )
+    """)
+
+    # Volunteers table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS volunteers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        skills TEXT,
+        radius INTEGER,
+        availability TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
 
